5bccbdd1d4b6cdb000e40f7ab5ea3937e0384477
Koz Ross <koz.ross@retro-freedom.nz>
2022-05-26 12:06:51 +1200

    Additive instances for PInteger

--- src/Plutarch/Numeric/Additive.hs
+++ src/Plutarch/Numeric/Additive.hs
@@ -4,6 +4,10 @@ module Plutarch.Numeric.Additive (
     AdditiveMonoid (..),
 ) where
 
+import Plutarch (Term)
+import Plutarch.Integer (PInteger)
+import qualified Prelude
+
 -- | @since 1.0.0
 class AdditiveSemigroup a where
     (+) :: a -> a -> a
@@ -11,3 +15,11 @@ class AdditiveSemigroup a where
 -- | @since 1.0.0
 class (AdditiveSemigroup a) => AdditiveMonoid a where
     zero :: a
+
+-- | @since 1.0.0
+instance AdditiveSemigroup (Term s PInteger) where
+    (+) = (Prelude.+)
+
+-- | @since 1.0.0
+instance AdditiveMonoid (Term s PInteger) where
+    zero = 0
